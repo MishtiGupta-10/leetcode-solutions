136093b84d46e3e707176ffb433f24a2c319e6de
MishtiGupta-10 <guptamishti87@gmail.com>
2025-08-30 08:02:47 +0000

    Sync LeetCode submission - Reverse Integer - Runtime - 0 ms (100.00%), Memory - 8.7 MB (21.06%)

---
+++ my-solutions/0007-reverse-integer/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        long rev=0;
+        int digit;
+
+        while (x!=0)
+        {
+            digit = x % 10;
+            rev = rev*10 + digit;
+
+            x = x/10;
+        }
+
+        if(rev < INT_MIN || rev > INT_MAX)
+        return 0;
+
+        return (int)rev;
+
+
+        
+    }
+};
