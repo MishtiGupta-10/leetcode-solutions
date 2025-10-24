c9eafdad7c46d8e1815a7ea8992c68bd03e51944
MishtiGupta-10 <guptamishti87@gmail.com>
2025-10-24 19:32:24 +0000

    Sync LeetCode submission - Palindrome Number - Runtime - 4 ms (30.63%), Memory - 8.7 MB (36.82%)

--- my-solutions/0009-palindrome-number/solution.cpp
+++ my-solutions/0009-palindrome-number/solution.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
-    int getReverse (int x)
-    {
-        long rev = 0;
-        int digit;
-
-        while(x!=0)
-        {
-            digit = x%10;
-            rev = rev*10 + digit;
-            x = x/10;
-        }
-        return rev;
-    }
     bool isPalindrome(int x) {
-        int reverse;
-        if (x < 0)
-        {
-            return false;
-        }
-        else 
-        {
-            reverse = getReverse(x);
-            if(reverse == x)
-            {
-                return true;
-            }
-            else 
-            {
-                return false;
-            }
+        int num ;
+        int rev = 0;
+        num = x;
+        if(x<0) return false;
+        while (x != 0) {
+            int rem = x % 10;
+            if (rev > INT_MAX / 10 || rev == INT_MAX && rem == 7)
+                return 0;
+            if (rev < INT_MIN / 10 || rev == INT_MIN && rem == -8)
+                return 0;
+            rev = rev * 10 + (x % 10);
+            x = x / 10;
         }
 
-        if (reverse < INT_MIN || reverse > INT_MAX)
-        return false;
+        if(num != rev)
+            return false;
+        else 
+            return true;
     }
 };
