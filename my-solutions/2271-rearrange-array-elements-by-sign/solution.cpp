94a30d77f1a2b60788c1bca77b276d840130bdec
MishtiGupta-10 <guptamishti87@gmail.com>
2026-03-28 15:51:57 +0000

    Sync LeetCode submission - Rearrange Array Elements by Sign - Runtime - 0 ms (100.00%), Memory - 128.3 MB (78.11%)

---
+++ my-solutions/2271-rearrange-array-elements-by-sign/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        int pos=0;
+        int neg = 1;
+        vector<int> arr(n);
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] >= 0)
+            {
+                arr[pos] = nums[i];
+                pos = pos+2;
+            }
+            else 
+            {
+                arr[neg] = nums[i];
+                neg = neg + 2;
+            }
+        }
+
+        return arr;
+    }
+};
