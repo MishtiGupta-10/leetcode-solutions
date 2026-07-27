50cb5b5cfd2cf4fe7ceaea7bd709d5f8b86db986
MishtiGupta-10 <guptamishti87@gmail.com>
2026-07-27 17:24:00 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement - Runtime - 7 ms (44.23%), Memory - 11 MB (42.05%)

---
+++ my-solutions/0424-longest-repeating-character-replacement/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        
+        unordered_map<int, int> freq;
+
+        int left = 0;
+
+        int max_freq =0;
+
+        int max_len = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            freq[s[right]]++;
+
+            max_freq = max(max_freq, freq[s[right]]);
+
+            while((right-left+1)-max_freq > k){
+                freq[s[left]]--;
+
+                left++;
+            }
+
+            max_len = max((right-left+1), max_len);
+        }
+
+        return max_len;
+    }
+};
