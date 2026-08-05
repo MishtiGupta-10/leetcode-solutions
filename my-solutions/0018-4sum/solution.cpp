8f73854eb6657f551354f9db4ca0a6528b89de50
MishtiGupta-10 <guptamishti87@gmail.com>
2026-08-05 20:15:42 +0000

    Sync LeetCode submission - 4Sum - Runtime - 26 ms (24.98%), Memory - 17.5 MB (23.86%)

--- my-solutions/0018-4sum/solution.cpp
+++ my-solutions/0018-4sum/solution.cpp
@@ -2,46 +2,40 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
-        int n = nums.size();
-
-        vector<vector<int>> result;
+        int n = nums.size() ; 
+        
+        vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
 
-        for(int i=0; i<n-3; i++){
+        for(int i=0; i<n; i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-
-            for(int j=i+1; j<n-2; j++){
+            for(int j=i+1; j<n; j++){
                 if(j>i+1 && nums[j] == nums[j-1]) continue;
 
                 int left = j+1;
                 int right = n-1;
 
                 while(left < right){
-                    long long sum = 1LL * nums[i] + nums[j] + nums[left] + nums[right];
+                    long long  sum = 1ll*nums[i] + 1ll*nums[j] + 1ll*nums[left] + 1ll*nums[right];
 
                     if(sum == target){
-                        result.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+
                         left++;
                         right--;
-                        while(left < right && nums[left] == nums[left-1]) left++;
-                        while(left < right && nums[right]  == nums[right+1]) right--;
+
+                        while(left < n && nums[left] == nums[left-1]) left++;
+                        while(right > 0 && nums[right] == nums[right+1]) right--;
                     }
                     else if(sum < target){
                         left++;
-                        while(left < right && nums[left] == nums[left-1]) left++;
                     }
-                    else{
-                        right--;
-                        while(left < right && nums[right]  == nums[right+1]) right--;
-                    }
-
-
+                    else right--;
                 }
-
             }
         }
 
-        return result;
+        return ans;
     }
 };
