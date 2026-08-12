8259c90f3bb26610881c1deeec2ba517ac6cffeb
MishtiGupta-10 <guptamishti87@gmail.com>
2026-08-12 21:12:50 +0000

    Sync LeetCode submission - Next Greater Element I - Runtime - 0 ms (100.00%), Memory - 13 MB (15.73%)

--- my-solutions/0496-next-greater-element-i/solution.cpp
+++ my-solutions/0496-next-greater-element-i/solution.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
-        vector<int> ans;
-
-        ans = nums2;
-
         int n = nums1.size();
         int m = nums2.size();
 
+        vector<int> ans;
 
-        for(int i = m-1; i>=0; i--){
-            while(!st.empty() && (st.top() <= nums2[i])){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i] = -1;
-            }
-            else{
-                ans[i] = st.top();
-            }
-            st.push(nums2[i]);
-        }
+        stack<int> st;
+        vector<int> res(m);
 
         unordered_map<int, int> mp;
 
-        for(int i=0; i<m; i++){
-            mp[nums2[i]] = ans[i];
+        for(int j=0; j<m; j++){
+            mp[nums2[j]] = j;
+        }
+
+        for(int j=m-1; j>=0; j--){
+            while(!st.empty() && st.top()<=nums2[j]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                res[j] = -1;
+            }
+            else{
+                res[j] = st.top();
+            }
+
+            st.push(nums2[j]);
         }
 
         for(int i=0; i<n; i++){
-            nums1[i] = mp[nums1[i]];
+            if(mp.find(nums1[i]) != mp.end()){
+                ans.push_back(res[mp[nums1[i]]]);
+            }
         }
 
-        return nums1;
+        return ans;
     }
 };
