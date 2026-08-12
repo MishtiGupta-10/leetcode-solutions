1a087922611a1e07bf60ab57e728729aa89c5280
MishtiGupta-10 <guptamishti87@gmail.com>
2026-08-12 21:25:36 +0000

    Sync LeetCode submission - Length of Longest Subarray With at Most K Frequency - Runtime - 69 ms (37.17%), Memory - 149.3 MB (84.13%)

---
+++ my-solutions/3225-length-of-longest-subarray-with-at-most-k-frequency/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxLen = 0;
+
+        unordered_map<int, int> freq;
+        int left = 0;
+        int len;
+
+        for(int right = 0; right <n; right++){
+            freq[nums[right]]++;
+                while(freq[nums[right]] > k){
+                    freq[nums[left]]--;
+                    left++;
+                }
+            
+            maxLen = max(maxLen, right-left+1);
+        }
+        return maxLen;
+
+    }
+};
